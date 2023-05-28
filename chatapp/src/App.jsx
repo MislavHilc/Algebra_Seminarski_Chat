1423cd69968e99f2b283724ec09e069e8657cfa0
Mislav <hilc84@gmail.com>
2023-05-28 18:39:19 +0200

    Testna poruka prikazana

--- chatapp/src/App.jsx
+++ chatapp/src/App.jsx
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Messages from "./Messages";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function randomName() {
+  // Random name generation logic
+}
+
+function randomColor() {
+  // Random color generation logic
+}
+
+class App extends Component {
+  state = {
+    messages: [
+      {
+        text: "This is a test message!",
+        member: {
+          clientData: {
+            color: "blue",
+            username: "bluemoon"
+          }
+        }
+      }
+    ],
+    member: {
+      clientData: {
+        username: randomName(),
+        color: randomColor()
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Messages messages={this.state.messages} currentMember={this.state.member} />
+      </div>
+    );
+  }
 }
 
 export default App;
