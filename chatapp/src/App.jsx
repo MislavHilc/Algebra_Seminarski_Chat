2bdbc739a57c28683f0b3c5c67f1289e5b93a851
Mislav <hilc84@gmail.com>
2023-05-28 18:57:09 +0200

    Povezano sa Scaledronom

--- chatapp/src/App.jsx
+++ chatapp/src/App.jsx
@@ -12,17 +12,7 @@ function randomColor() {
 
 class App extends Component {
   state = {
-    messages: [
-      {
-        text: "This is a test message!",
-        member: {
-          clientData: {
-            color: "blue",
-            username: "bluemoon"
-          }
-        }
-      }
-    ],
+    messages: [],
     member: {
       clientData: {
         username: randomName(),
@@ -49,12 +39,31 @@ class App extends Component {
   }
 
   onSendMessage = (message) => {
-    const messages = this.state.messages
-    messages.push({
-      text: message,
-      member: this.state.member
-    })
-    this.setState({messages: messages})
+    this.drone.publish({
+      room: "observable-room",
+      message
+    });
+  }
+
+  constructor() {
+    super();
+    this.drone = new window.Scaledrone("NmKR0z2Yk7epHuQY", {
+      data: this.state.member
+    });
+    this.drone.on('open', error => {
+      if (error) {
+        return console.error(error);
+      }
+      const member = {...this.state.member};
+      member.id = this.drone.clientId;
+      this.setState({member});
+      const room = this.drone.subscribe("observable-room");
+      room.on('data', (data, member) => {
+        const messages = this.state.messages;
+        messages.push({member, text: data});
+        this.setState({messages});
+      });
+    });
   }
 }
 
